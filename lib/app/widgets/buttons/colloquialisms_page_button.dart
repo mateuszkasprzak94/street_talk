64eb0739a96fb9bf10af8a34e543bf857569ea9f
Mateusz Kasprzak <mateusz.kasprzak17@gmail.com>
2023-12-18 14:32:35 +0100

    Change IconButton to InkWell

--- lib/app/widgets/buttons/colloquialisms_page_button.dart
+++ lib/app/widgets/buttons/colloquialisms_page_button.dart
@@ -18,57 +18,62 @@ class ColloquialismsButtonWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 20) +
-          const EdgeInsets.only(top: 10),
-      width: double.infinity,
-      height: 200,
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.grey),
-        color: Colors.white,
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.1),
-            spreadRadius: 5,
-            blurRadius: 7,
-            offset: const Offset(4, 8),
-          ),
-        ],
-      ),
-      child: Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
-        Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: SizedBox(
-                height: 90,
-                width: 90,
-                child: image,
-              ),
+    return InkWell(
+      onTap: onPressed,
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 20) +
+            const EdgeInsets.only(top: 10),
+        width: double.infinity,
+        height: 200,
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              spreadRadius: 5,
+              blurRadius: 7,
+              offset: const Offset(4, 8),
             ),
           ],
         ),
-        Expanded(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        child: Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+          Column(
             children: [
-              Text(
-                polishTitle,
-                style: GoogleFonts.lora(fontSize: 25),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SizedBox(
+                  height: 90,
+                  width: 90,
+                  child: image,
+                ),
               ),
-              Text(
-                spanishTitle,
-                style: GoogleFonts.lora(
-                    fontSize: screenWidth / 19,
-                    fontStyle: FontStyle.italic,
-                    color: Colors.grey),
-              )
             ],
           ),
-        ),
-        IconButton(
-            onPressed: onPressed, icon: const Icon(Icons.arrow_forward_ios))
-      ]),
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text(
+                  polishTitle,
+                  style: GoogleFonts.lora(fontSize: 25),
+                ),
+                Text(
+                  spanishTitle,
+                  style: GoogleFonts.lora(
+                      fontSize: screenWidth / 19,
+                      fontStyle: FontStyle.italic,
+                      color: Colors.grey),
+                )
+              ],
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Icon(Icons.arrow_forward_ios),
+          )
+        ]),
+      ),
     );
   }
 }
