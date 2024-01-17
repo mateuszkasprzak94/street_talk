46cbe5dd39c4a70fd0cb4c87886515fd291a207c
Mateusz Kasprzak <mateusz.kasprzak17@gmail.com>
2024-01-17 12:07:17 +0100

    Add Animation to FlashCards Page

--- lib/app/features/pages/flashcards_page/flashcards_page.dart
+++ lib/app/features/pages/flashcards_page/flashcards_page.dart
@@ -4,6 +4,7 @@ import 'package:street_talk/app/features/pages/flashcards_page/flashcards_sets_p
 import 'package:street_talk/app/features/pages/flashcards_page/flashcards_sets_page/flashcards_sets_page_content/set_three/set_three.dart';
 import 'package:street_talk/app/features/pages/flashcards_page/flashcards_sets_page/flashcards_sets_page_content/set_two/set_two.dart';
 import 'package:street_talk/app/widgets/drawer/drawer.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 
 class FlashCardsPage extends StatelessWidget {
   const FlashCardsPage({super.key});
@@ -52,52 +53,58 @@ class FlashCardsPage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const SizedBox(height: 20),
-            FloatingActionButton.large(
-              heroTag: 'btn1',
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (_) => const FlashCardsSetOne(),
-                  ),
-                );
-              },
-              backgroundColor: const Color(0xFFc60b1e),
-              child: const Text(
-                'Zestaw 1',
-                style: TextStyle(color: Colors.white),
-              ),
+            Animate(
+              child: FloatingActionButton.large(
+                heroTag: 'btn1',
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (_) => const FlashCardsSetOne(),
+                    ),
+                  );
+                },
+                backgroundColor: const Color(0xFFc60b1e),
+                child: const Text(
+                  'Zestaw 1',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ).animate().fade(delay: 500.ms, duration: 1000.ms).slideX(),
             ),
             const SizedBox(height: 20),
-            FloatingActionButton.large(
-              heroTag: 'btn2',
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (_) => const FlashCardsSetTwo(),
-                  ),
-                );
-              },
-              backgroundColor: const Color(0xFFc60b1e),
-              child: const Text(
-                'Zestaw 2',
-                style: TextStyle(color: Colors.white),
-              ),
+            Animate(
+              child: FloatingActionButton.large(
+                heroTag: 'btn2',
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (_) => const FlashCardsSetTwo(),
+                    ),
+                  );
+                },
+                backgroundColor: const Color(0xFFc60b1e),
+                child: const Text(
+                  'Zestaw 2',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ).animate().fade(delay: 1000.ms, duration: 1000.ms).slideX(),
             ),
             const SizedBox(height: 20),
-            FloatingActionButton.large(
-              heroTag: 'btn3',
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (_) => const FlashCardsSetThree(),
-                  ),
-                );
-              },
-              backgroundColor: const Color(0xFFc60b1e),
-              child: const Text(
-                'Zestaw 3',
-                style: TextStyle(color: Colors.white),
-              ),
+            Animate(
+              child: FloatingActionButton.large(
+                heroTag: 'btn3',
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (_) => const FlashCardsSetThree(),
+                    ),
+                  );
+                },
+                backgroundColor: const Color(0xFFc60b1e),
+                child: const Text(
+                  'Zestaw 3',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ).animate().fade(delay: 1500.ms, duration: 1000.ms).slideX(),
             ),
           ],
         ),
