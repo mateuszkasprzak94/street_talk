54f0d65853065afc410a9b76ac2e81e4111aa0ca
Mateusz Kasprzak <mateusz.kasprzak17@gmail.com>
2024-01-19 14:08:51 +0100

    Add AppBar

--- lib/app/drawer/drawer_content/quiz/quiz_home_page.dart
+++ lib/app/drawer/drawer_content/quiz/quiz_home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:street_talk/app/core/constants/constants.dart';
 
 class QuizHomePage extends StatefulWidget {
   const QuizHomePage({super.key});
@@ -10,9 +12,38 @@ class QuizHomePage extends StatefulWidget {
 class _QuizHomePageState extends State<QuizHomePage> {
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Quiz'),
+        centerTitle: true,
+        actions: const [
+          Padding(
+            padding: EdgeInsets.only(right: 10),
+            child: CircleAvatar(
+              radius: 30,
+              foregroundImage: AssetImage('assets/images/logo.jpg'),
+            ),
+          )
+        ],
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(70),
+          child: Container(
+            height: 60,
+            width: double.infinity,
+            decoration: const BoxDecoration(
+              color: kRedColor,
+            ),
+            child: Center(
+              child: Text(
+                'Pora na QUIZ!',
+                style: GoogleFonts.bebasNeue(
+                    letterSpacing: 2,
+                    color: Colors.white,
+                    fontSize: screenWidth / 12),
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
