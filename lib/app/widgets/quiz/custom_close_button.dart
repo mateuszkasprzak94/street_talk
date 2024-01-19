127f0a155546654adbdf864e832d81589f253231
Mateusz Kasprzak <mateusz.kasprzak17@gmail.com>
2024-01-19 14:09:27 +0100

    Create CustomCloseButton

--- lib/app/widgets/quiz/custom_close_button.dart
+++ lib/app/widgets/quiz/custom_close_button.dart
@@ -1,1 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:street_talk/app/core/constants/constants.dart';
 
+class CustomCloseButton extends StatelessWidget {
+  const CustomCloseButton({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        border: Border.all(color: kYellowColor),
+      ),
+      child: GestureDetector(
+        onTap: () {
+          Navigator.pop(context);
+        },
+        child: const Icon(
+          Icons.close_rounded,
+          color: kRedColor,
+          size: 30,
+        ),
+      ),
+    );
+  }
+}
