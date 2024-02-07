6745a7061408f69fbc3c4c9d8591a9419ef7ddb7
Mateusz Kasprzak <mateusz.kasprzak17@gmail.com>
2024-02-07 11:26:54 +0100

    Implements Bloc

--- lib/app/drawer/drawer_content/spanish_events/spanish_events_content.dart
+++ lib/app/drawer/drawer_content/spanish_events/spanish_events_content.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:street_talk/app/domain/models/spanish_events_details_model.dart';
+import 'package:street_talk/app/drawer/drawer_content/spanish_events/cubit/events_cubit.dart';
+import 'package:street_talk/app/injection_container.dart';
 import 'package:street_talk/app/widgets/events/custom_pop_button.dart';
 
 class EventsContent extends StatelessWidget {
@@ -11,24 +14,32 @@ class EventsContent extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Hero(
-            tag: eventCardDetailList[selectedIndex].iconTag,
-            child: ClipRRect(
-              borderRadius: const BorderRadius.only(
-                bottomLeft: Radius.circular(50),
-                bottomRight: Radius.circular(50),
-              ),
-              child: SizedBox(
-                  child: Image.asset(
-                      eventCardDetailList[selectedIndex].iconAssetName)),
-            ),
-          ),
-          const SizedBox(height: 50),
-          const CustomPopButton(),
-        ],
+      child: BlocProvider<EventsCubit>(
+        create: (context) => getIt()
+          ..fetchEvents(category: eventCardDetailList[selectedIndex].category),
+        child: BlocBuilder<EventsCubit, EventsState>(
+          builder: (context, state) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Hero(
+                  tag: eventCardDetailList[selectedIndex].iconTag,
+                  child: ClipRRect(
+                    borderRadius: const BorderRadius.only(
+                      bottomLeft: Radius.circular(50),
+                      bottomRight: Radius.circular(50),
+                    ),
+                    child: SizedBox(
+                        child: Image.asset(
+                            eventCardDetailList[selectedIndex].iconAssetName)),
+                  ),
+                ),
+                const SizedBox(height: 50),
+                const CustomPopButton(),
+              ],
+            );
+          },
+        ),
       ),
     );
   }
