5dfb70e14f588f844aa49b81d017d94ac2ecef35
Mateusz Kasprzak <mateusz.kasprzak17@gmail.com>
2024-02-23 09:58:57 +0100

    Add setPreferredOrientations

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,13 @@ import 'package:street_talk/app/app.dart';
 import 'package:street_talk/app/injection_container.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:flutter/services.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
